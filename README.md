acd10bf6fb258ca38f50b17fe0d4a733c0d2c767
Shadof <shadowvvf@mail.ru>
2023-11-21 16:15:53 +0300

    fixed arguments & updated readme

--- README.md
+++ README.md
@@ -1,47 +1,94 @@
-# PurplePipManager
+# Package Manager README
 
-PurplePipManager is a C++ program that manages Python packages. 
+This is a simple command-line package manager written in C++ to facilitate the installation, uninstallation, and listing of Python packages within a virtual environment. It also provides an option to resolve dependencies using the `--resolve` flag during installation.
 
 ## Usage
 
-`main.exe (command) [virtual_env_path] [options]`
+```
+Usage: main.exe (--command <command>) [--virtualenv <virtual_env_path>] [options]
+
+Commands:
+  install [--package <package_name>] [--version <version>] [--resolve]
+  uninstall [--package <package_name>]
+  list
+```
 
 ## Commands
 
-### Install
+### 1. Install
 
-To install a package:
+Installs a Python package within the specified virtual environment.
 
-`main.exe install (virtual_env_path) (package name) (version) [-resolve]`
+```
+main.exe --command install --package <package_name> [--version <version>] [--resolve] --virtualenv <virtual_env_path>
+```
 
-- `(virtual_env_path)`: The path to the virtual environment where the package will be installed.
-- `(package name)`: The name of the package to install.
-- `(version)`: The version of the package to install. If not specified, the latest version is installed.
-- `[-resolve]`: An optional flag that, if present, enables the use of the fast dependency resolver in pip.
+- `--package <package_name>`: Specifies the name of the package to be installed.
+- `--version <version>`: Optionally specifies the version of the package. If not provided, the latest version will be installed.
+- `--resolve`: Optionally resolves dependencies using the `--use-feature=fast-deps` flag during installation.
+- `--virtualenv <virtual_env_path>`: Specifies the path to the virtual environment where the package will be installed.
 
-### Uninstall
+### 2. Uninstall
 
-To uninstall a package:
+Uninstalls a previously installed Python package from the specified virtual environment.
 
-`main.exe uninstall (virtual_env_path) (package name)`
+```
+main.exe --command uninstall --package <package_name> --virtualenv <virtual_env_path>
+```
 
-- `(virtual_env_path)`: The path to the virtual environment from which the package will be uninstalled.
-- `(package name)`: The name of the package to uninstall.
+- `--package <package_name>`: Specifies the name of the package to be uninstalled.
+- `--virtualenv <virtual_env_path>`: Specifies the path to the virtual environment from which the package will be uninstalled.
 
-### List
+### 3. List
 
-To list installed packages:
+Lists all installed Python packages within the specified virtual environment.
 
-`main.exe list (virtual_env_path)`
+```
+main.exe --command list [--virtualenv <virtual_env_path>]
+```
 
-- `(virtual_env_path)`: The path to the virtual environment whose packages will be listed.
+- `--virtualenv <virtual_env_path>`: Optionally specifies the path to the virtual environment. If not provided, the global environment's packages will be listed.
 
-## Building the Project
+## Examples
 
-Use your preferred C++ compiler to compile the `main.cpp` file.
+### Install a package:
 
-Please note that this program uses the system's `pip` command to manage packages, so Python and pip must be installed on your system.
+```
+main.exe --command install --package requests --virtualenv path/to/venv
+```
 
-## Disclaimer
+### Install a specific version of a package:
 
-This program is a demonstration and not intended for production use.
+```
+main.exe --command install --package numpy --version 1.19.4 --virtualenv path/to/venv
+```
+
+### Install a package with dependency resolution:
+
+```
+main.exe --command install --package pandas --resolve --virtualenv path/to/venv
+```
+
+### Uninstall a package:
+
+```
+main.exe --command uninstall --package requests --virtualenv path/to/venv
+```
+
+### List installed packages:
+
+```
+main.exe --command list --virtualenv path/to/venv
+```
+
+### List global installed packages:
+
+```
+main.exe --command list
+```
+
+## Notes
+
+- Make sure to activate the virtual environment before running commands related to a specific environment.
+- If package installation fails, the tool will attempt to install the package from GitHub as a fallback.
+- For the `list` command, if no virtual environment is specified, it will list packages in the global environment.
--- main.cpp
+++ main.cpp
@@ -1,33 +1,69 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
+#include <cstring>
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cout << "Usage: main.exe (command) [virtual_env_path] [options]" << std::endl;
+    const char* usage = "Usage: main.exe (--command <command>) [--virtualenv <virtual_env_path>] [options]\n"
+        "Commands:\n"
+        "  install [--package <package_name>] [--version <version>] [--resolve]\n"
+        "  uninstall [--package <package_name>]\n"
+        "  list\n";
+
+    const char* command = nullptr;
+    const char* venvPath = nullptr;
+    const char* packageName = nullptr;
+    const char* version = nullptr;
+    bool resolveConflicts = false;
+
+    // Process command line arguments
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "--command") == 0 && i + 1 < argc) {
+            command = argv[++i];
+        }
+        else if (strcmp(argv[i], "--virtualenv") == 0 && i + 1 < argc) {
+            venvPath = argv[++i];
+        }
+        else if (strcmp(argv[i], "--package") == 0 && i + 1 < argc) {
+            packageName = argv[++i];
+        }
+        else if (strcmp(argv[i], "--version") == 0 && i + 1 < argc) {
+            version = argv[++i];
+        }
+        else if (strcmp(argv[i], "--resolve") == 0) {
+            resolveConflicts = true;
+        }
+    }
+
+    if (!command) {
+        std::cerr << "Error: Command not specified.\n" << usage;
         return 1;
     }
 
-    std::string command = argv[1];
-    std::string venvPath = argv[2];
+    if (!venvPath && strcmp(command, "list") != 0) {
+        std::cerr << "Error: Virtual environment path not specified.\n" << usage;
+        return 1;
+    }
 
-    if (command == "install") {
-        if (argc < 5) {
-            std::cout << "Usage: main.exe install (virtual_env_path) (package name) (version) [-resolve]" << std::endl;
+    if (strcmp(command, "install") == 0) {
+        if (!packageName) {
+            std::cerr << "Error: Package name not specified for install command.\n" << usage;
             return 1;
         }
 
-        std::string packageName = argv[3];
-        std::string version = (argc >= 6) ? argv[4] : "latest";
-        bool resolveConflicts = (argc >= 7) ? (std::string(argv[5]) == "-resolve") : false;
+        std::string installCommand = "source ";
+        installCommand += venvPath;
+        installCommand += "/bin/activate && pip install";
 
-        std::string installCommand = "source " + venvPath + "/bin/activate && pip install ";
         if (resolveConflicts) {
-            installCommand += "--use-feature=fast-deps ";
+            installCommand += " --use-feature=fast-deps";
         }
+
+        installCommand += " ";
         installCommand += packageName;
-        if (version != "latest") {
-            installCommand += "==" + version;
+
+        if (version) {
+            installCommand += "==";
+            installCommand += version;
         }
 
         int result = std::system(installCommand.c_str());
@@ -37,7 +73,7 @@ int main(int argc, char* argv[]) {
         }
         else {
             std::cout << "Package installation failed. Trying to install from GitHub..." << std::endl;
-            installCommand = "source " + venvPath + "/bin/activate && pip install git+https://github.com/ByteRise/purple-packages@" + packageName;
+            installCommand = "source " + std::string(venvPath) + "/bin/activate && pip install git+https://github.com/ByteRise/purple-packages@" + packageName;
             result = std::system(installCommand.c_str());
             if (result == 0) {
                 std::cout << "Package installation from GitHub successful!" << std::endl;
@@ -47,14 +83,16 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-    else if (command == "uninstall") {
-        if (argc < 4) {
-            std::cout << "Usage: main.exe uninstall (virtual_env_path) (package name)" << std::endl;
+    else if (strcmp(command, "uninstall") == 0) {
+        if (!packageName) {
+            std::cerr << "Error: Package name not specified for uninstall command.\n" << usage;
             return 1;
         }
 
-        std::string packageName = argv[3];
-        std::string uninstallCommand = "source " + venvPath + "/bin/activate && pip uninstall -y " + packageName;
+        std::string uninstallCommand = "source ";
+        uninstallCommand += venvPath;
+        uninstallCommand += "/bin/activate && pip uninstall -y ";
+        uninstallCommand += packageName;
 
         int result = std::system(uninstallCommand.c_str());
 
@@ -65,12 +103,21 @@ int main(int argc, char* argv[]) {
             std::cout << "Package uninstallation failed." << std::endl;
         }
     }
-    else if (command == "list") {
-        std::string listCommand = "source " + venvPath + "/bin/activate && pip list";
-        std::system(listCommand.c_str());
+    else if (strcmp(command, "list") == 0) {
+        if (venvPath) {
+            std::string listCommand = "source ";
+            listCommand += venvPath;
+            listCommand += "/bin/activate && pip list";
+            std::system(listCommand.c_str());
+        }
+        else {
+            std::string listCommand = "pip list";
+            std::system(listCommand.c_str());
+        }
     }
     else {
-        std::cout << "Unrecognized command: " << command << std::endl;
+        std::cerr << "Error: Unrecognized command: " << command << "\n" << usage;
+        return 1;
     }
 
     return 0;
